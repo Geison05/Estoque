ba23632e66ee5497236ffcbe602418e85dae1ad6
Geison Amorim <geisonam43@gmail.com>
2022-04-18 16:11:55 -0300

    Add project files.

---
+++ Estoque/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Project
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ","
+                + Quantidade
+                + " Unidade, Total $ "
+                + ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
